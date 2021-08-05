531aebf6c3ac5ad5616496f7d82191ed91fe752c
kamil184 <45402327+kamil184@users.noreply.github.com>
2021-08-05 18:25:44 +0300

    placeholder

--- app/src/main/java/com/kamil184/gifapp/RecyclerViewAdapter.java
+++ app/src/main/java/com/kamil184/gifapp/RecyclerViewAdapter.java
@@ -1,19 +1,21 @@
 package com.kamil184.gifapp;
 
-import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
-
 import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
 
@@ -22,6 +24,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     RecyclerViewAdapter(List<Gif> gifs) {
         this.gifs = gifs;
     }
+
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.gif_card, parent, false);
@@ -46,10 +49,10 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         private View view;
 
-        ViewHolder(View view){
+        ViewHolder(View view) {
             super(view);
             this.view = view;
-            gifView = (ImageView)view.findViewById(R.id.gif);
+            gifView = (ImageView) view.findViewById(R.id.gif);
             likedView = (ImageButton) view.findViewById(R.id.liked);
             likedView.setOnClickListener(button -> {
                 likedView.setSelected(!likedView.isSelected());
@@ -62,8 +65,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             });
         }
-        public void setGifView(String url){
-            Glide.with(view.getContext()).asGif().load(url).into(gifView);
+
+        public void setGifView(String url) {
+            DisplayMetrics displayMetrics = view.getContext().getResources().getDisplayMetrics();
+            float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+            float dpPlaceholderWidth = (dpWidth - 48) / 3; //48 = 6 * 8, where 6 is the number of gaps, 8 is the margin
+            float dpPlaceholderHeight = 150;
+            int placeholderWidth = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpPlaceholderWidth, displayMetrics));
+            int placeholderHeight = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dpPlaceholderHeight, displayMetrics));
+
+            Glide.with(view.getContext()).asGif().placeholder(new SizableColorDrawable(placeholderWidth, placeholderHeight)).load(url).into(gifView);
         }
     }
 }
---
+++ app/src/main/java/com/kamil184/gifapp/SizableColorDrawable.java
@@ -0,0 +1,32 @@
+package com.kamil184.gifapp;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+
+public class SizableColorDrawable extends ColorDrawable {
+
+    int mWidth = -1;
+
+    int mHeight = -1;
+
+    public SizableColorDrawable(int color, int width, int height) {
+        super(color);
+
+        mWidth = width;
+        mHeight = height;
+    }
+
+    public SizableColorDrawable(int width, int height) {
+        super(Color.GRAY);
+
+        mWidth = width;
+        mHeight = height;
+    }
+
+    @Override public int getIntrinsicWidth() {
+        return mWidth;
+    }
+
+    @Override public int getIntrinsicHeight() {
+        return mHeight;
+    }
+}
